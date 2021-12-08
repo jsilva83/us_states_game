6ca8a78d15bfec1e9dab2b34dae614c7e6fe8c32
jsilva83 <jorge.h.silva@gmail.com>
2021-12-08 09:48:42 +0000

    v1.0 complete with exporting states to learn.

--- main.py
+++ main.py
@@ -7,20 +7,30 @@ screen.title('U.S. States Game')
 image = 'blank_states_img.gif'
 screen.addshape(image)
 turtle.shape(image)
-state_df = pd.read_csv('50_states.csv')
+states_df = pd.read_csv('50_states.csv')
 game_is_on = True
 nr_right_guesses = 0
-guesses_states_list = []
+guessed_states_list = []
 while game_is_on:
-    a_state = screen.textinput(title=f'Guesses {nr_right_guesses}/50', prompt='Enter a U.S. state name:').lower()
-    a_state = string.capwords(a_state, sep=None)
-    state_row_df = state_df[state_df['state'] == a_state]
+    # My version.
+    # a_state = screen.textinput(title=f'Guesses {nr_right_guesses}/50', prompt='Enter a U.S. state name:').lower()
+    # a_state = string.capwords(a_state, sep=None)
+    # Alternative.
+    a_state = screen.textinput(title=f'Guesses {nr_right_guesses}/50', prompt='Enter a U.S. state name:').title()
+    if a_state == 'Exit':
+        break
+    state_row_df = states_df[states_df['state'] == a_state]
     # Check if state exist in dataframe.
     if not state_row_df.empty:
-        a_state_name = state_row_df.iloc[0, 0]
-        a_state_x = state_row_df.iloc[0, 1]
-        a_state_y = state_row_df.iloc[0, 2]
-        if a_state_name not in guesses_states_list:
+        # My version.
+        # a_state_name = state_row_df.iloc[0, 0]
+        # a_state_x = state_row_df.iloc[0, 1]
+        # a_state_y = state_row_df.iloc[0, 2]
+        # Alternative
+        a_state_name = state_row_df['state'].item()
+        a_state_x = state_row_df['x'].item()
+        a_state_y = state_row_df['y'].item()
+        if a_state_name not in guessed_states_list:
             # Write the name of the state in the image.
             a_turtle = turtle.Turtle()
             a_turtle.penup()
@@ -30,10 +40,14 @@ while game_is_on:
             # Increase the number of right guesses.
             nr_right_guesses += 1
             # Add the state to a list of guessed states.
-            guesses_states_list.append(a_state_name)
+            guessed_states_list.append(a_state_name)
         if nr_right_guesses == 50:
             game_is_on = False
-screen.exitonclick()
+# Write the missing states to file 'states_to_learn.csv'
+# Create an intersection between the guessed_states_list and the states_df dataframe
+non_guessed_states_df = states_df[~states_df['state'].isin(guessed_states_list)]
+non_guessed_states_sr = non_guessed_states_df['state']
+non_guessed_states_sr.to_csv('states_to_learn.csv')
 
 # This piece of code gets the coordinates on the screen.
 # def get_mouse_click(x, y):
